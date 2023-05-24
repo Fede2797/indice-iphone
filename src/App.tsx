2801cbf797ea3efb9f67addd0999cea903dbedf0
Federico Passarelli <federico2797@gmail.com>
2023-05-24 19:44:00 -0300

    First commit

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+export function App() {
+
+  return (
+    <main className="bg-yellow min-h-screen">
+      {/* Main container */}
+      <section className="flex flex-col justify-center items-center">
+        <h1 className="MabryPro font-MabryPro text-6xl">Índice iPhone</h1>
+        <ul className="flex gap-1">
+          <li>jubilacion</li>
+          <li>sueldo mínimo</li>
+          <li>pesos arg</li>
+          <li>usd</li>
+          <li>btc</li>
+          <li>cafecitos</li>
+        </ul>
+      </section>
+    </main>
+  )
+}
